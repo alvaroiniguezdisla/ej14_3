47af6e8b6ac8bc87829ec95f90c0568c32cbd4a6
Álvaro <ainiguezdislaa@gmail.com>
2023-02-26 17:30:16 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,8 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej14_3;
+public class Principal {
+    public static void main(String[] args) {
+        int[] lista = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("La suma de los pares es: " + dominio.ej14_3.sumapares(lista));
+    }
 }
--- src/dominio/ej14_3.java
+++ src/dominio/ej14_3.java
@@ -1,2 +1,8 @@
-package dominio;public class ej14_3 {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej14_3 {
+    public static int sumapares(int[] lista){
+        return Stream.iterate(0,i->i+1).limit(lista.length).map(i->lista[i]).filter(i->i%2==0).reduce(0,(a,b)->a+b);
+    }
 }
